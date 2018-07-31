30328866a0232b3045aea4e69d62b9e76923e3f2
Sammy Eang <sammyeang@gmail.com>
2018-07-31 09:58:08 -0400

    Day 1 some stuff

---
+++ Week1/JavaFundamentals/src/com/revature/datatypes/Primitives.java
@@ -0,0 +1,27 @@
+package com.revature.datatypes;
+
+public class Primitives {
+
+	/*
+	 * Java has 8 primitive datatypes, which represent raw
+	 * data in an organized form. int, bool, byte, char,
+	 * short, double, long, float
+	 * 
+	 * When a number/string/array/etc is written out, it is called
+	 * a literal.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char ch = 'a';
+		
+		int var; //declaring variable
+		var = 5; //initializing variable
+		var = 10; //reassigning variable
+		
+		int x = 10; //declaring and initializing variable
+		//cannot declare the same var twice in the same scope
+		
+	}
+
+}
---
+++ Week1/JavaFundamentals/src/com/revature/into/HelloWorld.java
@@ -0,0 +1,32 @@
+package com.revature.into;
+
+/* The first line of any Java class 
+ * is the package that it is in, then
+ * the import statements/resources
+ */
+
+//[access modifier] [non-access modifier] class [className] {}
+public class HelloWorld {
+	/* 
+	 * Access modifiers control the level of security
+	 * that other classes can access.
+	 * public - any class can access this entity
+	 * protected - any class in this package or its subclasses can access
+	 * __ (default/package) - only accessible from package
+	 * private - only accessible within the class (nested class)
+	 */
+	
+	//this is a comment
+	
+	//[access modifier] [non-access modifier] [return type*] [methodName]([parameters])
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		
+		//ctrl + space to auto-complete a line
+		//ctrl + backspace to remove an entire line
+		
+		
+	}
+	
+}
