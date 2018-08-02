8ef63fe4c3fc05107ae2705103477b598257faaf
Ian Johnson <ianprime0509@gmail.com>
2018-08-02 14:57:37 -0400

    Add another homework question

---
+++ Ian_Johnson_Code/Assignments/Week1Homework/src/com/iantimothyjohnson/homework/question15/Question15.java
@@ -0,0 +1,17 @@
+package com.iantimothyjohnson.homework.question15;
+
+/**
+ * Write a program that defines an interface having the following methods:
+ * addition, subtraction, multiplication, and division. Create a class that
+ * implements this interface and provides appropriate functionality to carry out
+ * the required operations. Hard code two operands in a test class having a main
+ * method that calls the implementing class.
+ * 
+ * @author Ian Johnson
+ */
+public class Question15 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
