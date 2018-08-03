e0a91cb2913fc549c80e489680a1ce90037a4ef0
DailyDataMiner <benjaminmartin@theitguy.me>
2018-08-03 08:33:56 -0400

    Progress on hw.

--- Benjamin_Martin_Code/src/com/revature/substring/Substring.java
+++ Benjamin_Martin_Code/src/com/revature/substring/Substring.java
@@ -1,14 +1,47 @@
 package com.revature.substring;
 
+import java.util.*;
+
 public class Substring {
+	
+	public static String getString(String s, String sub, int x) {
+		s = s.toLowerCase();
+		sub = sub.toLowerCase();
+		String checkString = "";
+		String newString = "";
+		int indexOf = 0;
+		
+		if (x > 0) {
+			for (int i = 0 ; i < x ; i++)
+				newString = newString + s.charAt(i);
+		}
+		
+		for (int i = x ; i < s.length() ; i++ ) {
+			char a = s.charAt(i);
+			if (a == sub.charAt(indexOf)) {
+				checkString = checkString + a;
+				indexOf++;
+				if (indexOf >= sub.length())
+					indexOf = 0;
+			}
+			else {
+				if (indexOf != 0)
+					newString = newString + checkString;
+				indexOf = 0;
+				newString = newString + a;
+			}
+		}
+		return newString;
+	}
 
 	public static void main(String[] args) {
 		
-		// Need to convert args[1] from a string to int
-		int index = Integer.parseInt(args[1]);
-		
-		// Just prints out the results from 0 to the index
-		// they don't want to print
-		System.out.println(args[0].substring(0, index));
+		Scanner sc = new Scanner(System.in);
+        String s = sc.next();,
+        String sub = sc.next();
+        int x = sc.nextInt();
+		String newString = getString(s, sub, x);
+		System.out.println(newString);
+		sc.close();
 	}
 }
