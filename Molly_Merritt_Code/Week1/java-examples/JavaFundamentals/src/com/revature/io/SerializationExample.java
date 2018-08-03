4345aa91e7af8f777cd9822178497c12739dd249
mollymerritt <merritt.mry@gmail.com>
2018-08-03 09:31:03 -0400

    changes

--- Molly_Merritt_Code/Week1/java-examples/JavaFundamentals/src/com/revature/io/SerializationExample.java
+++ Molly_Merritt_Code/Week1/java-examples/JavaFundamentals/src/com/revature/io/SerializationExample.java
@@ -1,5 +1,10 @@
 package com.revature.io;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class SerializationExample {
 	
 	/*
@@ -19,11 +24,19 @@ public class SerializationExample {
 	 */
 
 	public static void main(String[] args) {
-		
+		Student s = new Student("Gen", "gab12@duke.edu", 100);
+		serializeObject(s);
 	}
 	
 	static void serializeObject(Object o) {
-		
+		try(ObjectOutputStream oos =
+				new ObjectOutputStream(new FileOutputStream("src/files/serial.txt"))){
+			oos.writeObject(o);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
