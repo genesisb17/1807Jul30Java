73a21875b0ceedaf0cc3c159cd679d8987750549
mollymerritt <merritt.mry@gmail.com>
2018-08-08 09:26:43 -0400

    add more files

--- Molly_Merritt_Code/Week1/java-examples/HomeworkWeek1/src/q15/Q15.java
+++ Molly_Merritt_Code/Week1/java-examples/HomeworkWeek1/src/q15/Q15.java
@@ -8,6 +8,36 @@ package q15;
  * method that calls the implementing class.
  */
 
-public class Q15 {
+public class Q15 implements Q15able {
+	
+	public static void main(String[] args) {
+		Q15 q15 = new Q15();
+		double d1 = 5;
+		double d2 = 10;
+		double dAdd = q15.addition(d1,d2);
+		double dMultiply = q15.multiplication(d1, d2);
+		if (dAdd == d1+d2) {
+			System.out.println("Addition passes test");
+		} else { System.out.println("Addition fails test"); }
+		if (dMultiply == d1*d2) {
+			System.out.println("Multiplication passes test");
+		} else { System.out.println("Multiplication fails test"); }
+	}
+
+	public double addition(double d1, double d2) {
+		return d1+d2;
+	}
+
+	public double subtraction(double d1, double d2) {
+		return d1-d2;
+	}
+
+	public double multiplication(double d1, double d2) {
+		return d1*d2;
+	}
+
+	public double division(double d1, double d2) {
+		return d1/d2;
+	}
 
 }
---
+++ Molly_Merritt_Code/Week1/java-examples/HomeworkWeek1/src/q15/Q15able.java
@@ -0,0 +1,10 @@
+package q15;
+
+public interface Q15able {
+	
+	double addition(double d1, double d2);
+	double subtraction(double d1, double d2);
+	double multiplication(double d1, double d2);
+	double division(double d1, double d2);
+
+}
