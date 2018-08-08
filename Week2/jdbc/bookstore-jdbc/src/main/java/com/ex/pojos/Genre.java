f33cdbc16aa55f627f62199c306b685e5f477215
genesisb17 <genesis.bonds@gmail.com>
2018-08-08 15:28:29 -0400

    beginnings og jdbc

---
+++ Week2/jdbc/bookstore-jdbc/src/main/java/com/ex/pojos/Genre.java
@@ -0,0 +1,18 @@
+package com.ex.pojos;
+
+public class Genre {
+	
+	private int id;
+	private String name;
+	
+	public Genre() {}
+	
+	public Genre(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	
+
+}
