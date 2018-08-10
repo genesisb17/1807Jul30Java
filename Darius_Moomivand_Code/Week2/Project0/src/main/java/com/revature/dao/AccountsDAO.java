537f3bdbd45613e887e41f76836bf1330587866a
Darius <dariusmoomivand@outlook.com>
2018-08-10 17:09:57 -0400

    Added ConnectionFactory

--- Darius_Moomivand_Code/Week2/Project0/src/main/java/com/revature/dao/AccountsDAO.java
+++ Darius_Moomivand_Code/Week2/Project0/src/main/java/com/revature/dao/AccountsDAO.java
@@ -1,14 +1,46 @@
 package com.revature.dao;
 
-import com.revature.accounts.*;
 import com.revature.pojo.Accounts;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class AccountsDAO {
+public class AccountsDAO implements Dao<Accounts, Integer>{
 
-	public static List<Accounts> returnAll(){
+	@Override
+	public List<Accounts> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Accounts findOne(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Accounts save(Accounts obj) {
+/*		try(Connection conn = ConnectionFactory.getInstance().getConnection()){
+			String sql = "insert into accounts(accountType, type) values(?,?)";
+			String[] keys = {""
+*/		
+		return obj;
+	}
+
+	@Override
+	public Accounts update(Accounts obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Accounts obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 		
 		
 		return null;
---
+++ Darius_Moomivand_Code/Week2/Project0/src/main/java/com/revature/util/ConnectionFactory.java
@@ -0,0 +1,51 @@
+package com.revature.util;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionFactory {
+	
+	private static ConnectionFactory cf  = null;
+	private static Boolean build =true;
+	
+	private ConnectionFactory() {
+		build = false;
+	}
+	
+	public static synchronized ConnectionFactory getInstance() {
+		if(build) cf = new ConnectionFactory();
+		return cf;
+	}
+	
+	public Connection getConnections() {
+		Connection conn = null;
+		Properties prop = new Properties();
+		String path = "C://Users//Darius//my_git_repos//1807Jul30Java//Darius_Moomivand_Code//Week2//Project0//src//main//resources//application.properties";
+		try {
+			prop.load(new FileReader(path));
+			Class.forName(prop.getProperty("driver"));
+			
+			conn = DriverManager.getConnection(
+					prop.getProperty("url"),
+					prop.getProperty("usr"),
+					prop.getProperty("pwd"));
+
+					
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
