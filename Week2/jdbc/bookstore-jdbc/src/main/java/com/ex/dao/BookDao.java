d230ecd503cc8261964243cf18d808f7994626c5
genesisb17 <genesis.bonds@gmail.com>
2018-08-14 18:16:09 -0400

    adding more to jdbc example

--- Week2/jdbc/bookstore-jdbc/src/main/java/com/ex/dao/BookDao.java
+++ Week2/jdbc/bookstore-jdbc/src/main/java/com/ex/dao/BookDao.java
@@ -69,43 +69,53 @@ public class BookDao implements Dao<Book, Integer>{
 	// Book(id, isbn, title, price, g.getId());
 	@Override
 	public Book save(Book obj) {
-	Book book = new Book();
-		
 		try(Connection conn = ConnectionFactory
 				.getInstance().getConnection();){
 			conn.setAutoCommit(false);
-			String query = "insert into book(isbn, title, price, genre) "
+			String query = "insert into book(isbn, title, price, genre_id) "
 					+ "values(?,?, ?, ?)";
 			
 			String[] keys = new String[1];
 			keys[0] = "book_id";
-			
 			PreparedStatement ps = conn.prepareStatement(query, keys);
 			ps.setString(1, obj.getIsbn());
 			ps.setString(2, obj.getTitle());
 			ps.setDouble(3, obj.getPrice());
 			ps.setInt(4, obj.getGenreId());
-			
 			int rows = ps.executeUpdate();
-			
+			System.out.println(rows + " BOOKS ADDED");
 			if(rows != 0) {
 				ResultSet pk = ps.getGeneratedKeys();
 				while(pk.next()) {
-					book.setId(pk.getInt(1));
+					int id = pk.getInt(1);
+					obj.setId(id);
+					System.out.println("BOOK ID IS " + id);
 				}
-				
 				conn.commit();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return book;
+		return obj;
 	}
 
 	@Override
 	public Book update(Book obj) {
-		// TODO Auto-generated method stub
-		return null;
+		try(Connection conn = ConnectionFactory.
+				getInstance().getConnection()){
+			String sql = "update book" + 
+					"set title = ?, genre_id = ? " + 
+					"where book_id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1,  obj.getTitle());
+			ps.setInt(2, obj.getGenreId());
+			ps.setInt(3, obj.getGenreId());
+			int rows = ps.executeUpdate();
+			System.out.println(rows + " BOOKS UPDATED");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return obj;
 	}
 
 	@Override
--- Week2/jdbc/bookstore-jdbc/src/main/java/com/ex/main/App.java
+++ Week2/jdbc/bookstore-jdbc/src/main/java/com/ex/main/App.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 
 import com.ex.pojos.Book;
 import com.ex.pojos.Genre;
+import com.ex.service.BookService;
 import com.ex.service.GenreService;
 
 public class App {
 
 	static Scanner scanner = new Scanner(System.in);
 	static GenreService gService = new GenreService();
+	static BookService bService = new BookService();
 
 	public static void main(String[] args) {
 		/*
@@ -21,6 +23,14 @@ public class App {
 		System.out.println("WELCOME!\n"
 				+ "Ready to Read?!");
 		menu();
+		
+//		Dao dao = new BookDao();
+//		Book b = new Book();
+//		b.setGenreId(22);
+//		b.setIsbn("91849789");
+//		b.setTitle("Testing");
+//		b.setPrice(10);
+//		dao.save(b);
 	}
 
 	static void menu() {
@@ -41,7 +51,7 @@ public class App {
 		}
 		
 		switch(option) {
-		case 1:
+		case 1: viewBooks(); break;
 		case 2:
 			viewGenres();
 			break;
@@ -54,24 +64,46 @@ public class App {
 		}
 	}
 	
+	static void viewBooks() {
+		//call service layer --> dao layer getAll
+		//print out all books
+		//allow user to edit or update books from here
+	}
+	
 	static void addBook() {
 		System.out.println("Enter your book's title:");
 		String title = scanner.nextLine();
 		System.out.println("Enter the ISBN (10-digit number):");
+		/*
+		 * SHOULD DO SOME INPUT VALIDATION
+		 * make sure its 10 NUMBER digits -- input validation
+		 * 
+		 */
+		
 		String isbn = scanner.nextLine();
 		System.out.println("Enter the price:");
 		double price = Double.parseDouble(scanner.nextLine()); //SURROUND IN TRY CATCH FOR NFE
 		System.out.println("Select your Genre(Enter its number)");
 		List<Genre> genres = gService.getAll();
 		for(int i = 0; i < genres.size(); i++) {
-			System.out.println(i+1 + ": " + genres.get(i));
+			System.out.println(i+1 + ": " + genres.get(i).getName());
 		}
 		int genreIndex = Integer.parseInt(scanner.nextLine())-1;
 		int genreID = genres.get(genreIndex).getId();
 		Book b = new Book(isbn, title, price, genreID);
 		//call book service addBook() which calls dao addBook()
 		//WE HAVE MAINTAINED REFERENTIAL INTEGRITY
+		b = bService.addBook(b);
+		System.out.println(b.toString());
+		System.out.println("Now who wrote the book! If it is one of"
+				+ " our well known authors below, please enter the"
+				+ " number next to their name. If not, please "
+				+ "write No/N");
+		String in = scanner.nextLine();
 		
+		//if No, call add author, and automatically add that 
+		//author id to the book 
+	
 	}
 	static void viewGenres() {
 		System.out.println("Current Genres:");
