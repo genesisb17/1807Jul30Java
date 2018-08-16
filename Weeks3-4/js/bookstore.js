afcfab530a3d5d7bfdf0366a3ffcb6be923013f0
genesisb17 <genesis.bonds@gmail.com>
2018-08-16 11:36:15 -0400

    jquery stuff

--- Weeks3-4/js/bookstore.js
+++ Weeks3-4/js/bookstore.js
@@ -12,14 +12,40 @@ function loadGenres(){
         document.getElementById("genres").appendChild(element);
     }
 }
+var count = 1;
 
 function addBook(){
-    console.log("add book!\nTEST");
+    //get input values
     var isbn = document.getElementById("isbn").value;
     var title = document.getElementById("title").value;
     var price = document.getElementById("price").value;
     var el= document.getElementById("genres");
     var genre = el.options[el.selectedIndex].value;
+    var id = ++count;
 
-    console.log(genre);
+    //generate new DOM elements 
+    var row =  document.createElement("tr");
+    var cell1 = document.createElement("td");
+    var cell2 = document.createElement("td");
+    var cell3 = document.createElement("td");
+    var cell4 = document.createElement("td");
+    var cell5 = document.createElement("td");
+
+    //assign our cells appropriate data 
+    cell1.innerHTML  = id; //<td> id </td>
+    cell2.innerHTML = isbn;
+    cell3.innerHTML = title;
+    cell4.innerHTML = price;
+    cell5.innerHTML = genre;
+
+    //append elements to DOM
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+    row.appendChild(cell4);
+    row.appendChild(cell5);
+    
+    document.getElementById("booksTable").appendChild(row);
+
+
 }
