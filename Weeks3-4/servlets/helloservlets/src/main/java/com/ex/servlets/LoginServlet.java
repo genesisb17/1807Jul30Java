e09d80ce73a27fc13dad20c7ebdc316202196c70
genesisb17 <genesis.bonds@gmail.com>
2018-08-21 15:26:13 -0400

    more on forms w servlets

--- Weeks3-4/servlets/helloservlets/src/main/java/com/ex/servlets/LoginServlet.java
+++ Weeks3-4/servlets/helloservlets/src/main/java/com/ex/servlets/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.ex.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,9 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ex.pojos.User;
+import com.ex.service.DummyUserService;
+
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet{
 	
+	static DummyUserService uService = new DummyUserService();
 	
 	@Override
 	protected void doPost(HttpServletRequest req,
@@ -22,6 +27,22 @@ public class LoginServlet extends HttpServlet{
 		String pass = req.getParameter("password");
 		
 		System.out.println("LOGGING IN USER " + name  + ":" + pass);
+		
+		User u = uService.getByUsername(name);
+		
+		PrintWriter out = resp.getWriter();
+		if(u == null) {
+			out.println("Sorry, invalid username");
+		}
+		else if(!u.getPassword().equals(pass)) {
+			out.println("Sorry, invalid passsword");
+		}
+		else {
+			//valid login 
+			out.println("Welcome, " + name + "!");
+		}
+	
+	
 	}
 	
 
