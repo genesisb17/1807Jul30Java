73734c6fdf4eb6de8b5caeceee7bc81b32b31b75
DailyDataMiner <benjaminmartin@theitguy.me>
2018-08-22 01:28:14 -0400

    Got it to also run on Desktop.

--- helloservlets/src/main/java/com/ex/servlets/HelloServlet.java
+++ helloservlets/src/main/java/com/ex/servlets/HelloServlet.java
@@ -44,14 +44,10 @@ public class HelloServlet extends GenericServlet {
 	 */
 	@Override
 	public void service(ServletRequest req, 
-			ServletResponse res) throws ServletException, IOException {
-<<<<<<< HEAD
-		
+			ServletResponse res) throws ServletException, IOException {		
 		System.out.println("--- IN HELLOSERVLET service() -- count: " + counter);
-=======
 		String counter = null;
 		System.out.println("--- IN HELLOSERVLET service() -- count" + counter);
->>>>>>> 8a45ee28df4024c7025b4780b97ab6f3effdf9bd
 		
 		PrintWriter writer = res.getWriter();
 		
