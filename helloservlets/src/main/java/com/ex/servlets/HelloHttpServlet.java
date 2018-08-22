aebb983ae74c2167ed6af1754a37e4cf116d62b2
DailyDataMiner <benjaminmartin@theitguy.me>
2018-08-22 01:12:47 -0400

    bleh

--- helloservlets/src/main/java/com/ex/servlets/HelloHttpServlet.java
+++ helloservlets/src/main/java/com/ex/servlets/HelloHttpServlet.java
@@ -8,41 +8,56 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloHttpServlet extends HttpServlet {
+public class HelloHttpServlet extends HttpServlet{
 	
 	@Override
 	public void init() throws ServletException {
 		// TODO Auto-generated method stub
 		super.init();
-		System.out.println("IN HTTP SERVLET INIT");
+		System.out.println("IN HTTP SERLVET INIT");
 	}
 	
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
 		super.destroy();
-		System.out.println("IN HTTP SERVLET destroy");
+		System.out.println("IN HTTP DESTROY");
 	}
+	
 	
 	@Override
 	protected void doGet(HttpServletRequest req, 
-			HttpServletResponse resp) throws ServletException, IOException {
+			HttpServletResponse resp) 
+					throws ServletException, IOException {
+		System.out.println("IN HTTP SERVLET DOGET");
 		PrintWriter out = resp.getWriter();
 		resp.setContentType("text/html");
 		
+		//Servlet Config is any configuration detail about this particular servlet
 		String info = getServletConfig().getInitParameter("info");
 		out.write("<h1> Hello <b>Genesis</b> ! </h1>"
 				+ "<br> Random init params: <i>" + info + "</i>");
 	}
 	
-	
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+	protected void doPost(HttpServletRequest req, 
+			HttpServletResponse resp) 
+					throws ServletException, IOException {
+		
 		
 		PrintWriter out = resp.getWriter();
-		out.write("IN HTTP SERVLET POST METHOD");
+		out.write("IN HTTPSERVLET POST METHOD!");
 	}
 	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPut(req, resp);
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doDelete(req, resp);
+	}
 }
--- helloservlets/src/main/java/com/ex/servlets/HelloServlet.java
+++ helloservlets/src/main/java/com/ex/servlets/HelloServlet.java
@@ -11,41 +11,56 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-/**
- * @author Benjamin Martin
- *
- */
 public class HelloServlet extends GenericServlet {
-
-	/* (non-Javadoc)
-	 * @see javax.servlet.GenericServlet#service(javax.servlet.ServletRequest, javax.servlet.ServletResponse)
-	 */
+	
+	static int counter = 0;
+	/*
+		LIFECYCLE OF A SERVLET! init() service() destroy()
+		init is called by the container when the servlet is 
+		initialized. here you can configure things like 
+		parameters, loggers, etc. basically anything you want
+		to happen upon initialization of this servlet
 		
+		the container then calls the service() method every 
+		time a request is sent to the url-pattern that you have
+		assigned this servlet. basically this is the part
+		where the servlet "services" the request
+		
+		lastly, the container calls destroy which will deallocate
+		memory to the servlet. we often don't see this happen bc
+		we don't typically limit the lifetime of the servlet, 
+		and if we simply stop the app from running, this method 
+		call is not shown
+	 */
 	@Override
 	public void init() throws ServletException {
 		// TODO Auto-generated method stub
 		super.init();
 		System.out.println("INITIALIZING HELLO SERVLET");
 	}
-	
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.GenericServlet#service(javax.servlet.ServletRequest, javax.servlet.ServletResponse)
+	 */
 	@Override
 	public void service(ServletRequest req, 
 			ServletResponse res) throws ServletException, IOException {
-		System.out.println("--- IN HELLOSERVLET service() -- count" + counter);
+		
+		System.out.println("--- IN HELLOSERVLET service() -- count: " + counter);
 		
 		PrintWriter writer = res.getWriter();
 		
+		//SERVLET CONTEXT is any config details applying to the entire servlet container
 		String bio = getServletContext().getInitParameter("Bio");
 		
 		writer.println("HELLO WORLD! WELCOME TO JAVA SERVLETS!"
 				+ "About me: " + bio);
+
 	}
 	
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
 		super.destroy();
 		System.out.println("IN HELLOSERVLET destroy()");
 	}
-
 }
