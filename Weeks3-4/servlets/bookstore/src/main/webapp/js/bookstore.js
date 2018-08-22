33aec205e6a90973b5bda3bf8f5f01794b10e835
genesisb17 <genesis.bonds@gmail.com>
2018-08-22 16:11:22 -0400

    adding beginning of bookstore example

---
+++ Weeks3-4/servlets/bookstore/src/main/webapp/js/bookstore.js
@@ -0,0 +1,31 @@
+/**
+ *  Single JS script for entire bookstore app. 
+ *  Separate functionality into methods and keep as 
+ *  organized as possible.. as this will get lengthy
+ */
+
+window.onload=function(){
+	loadHomeView();
+	$('#homeNav').on('click', loadHomeView);
+	$('#booksNav').on('click', loadBooksView);
+}
+
+
+function loadHomeView(){
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		//just showing what we're getting back for now
+		//console.log(xhr.responseText);
+		if(xhr.readyState == 4 && xhr.status == 200){
+			$('#view').html(xhr.responseText);
+		}
+		
+	}
+	xhr.open("GET", "loadHome");
+	xhr.send();
+	
+}
+
+function loadBooksView(){
+	
+}
