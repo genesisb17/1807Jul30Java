1b587c30b0a27daed44d394ee1e257ae5f575e6e
mollymerritt <merritt.mry@gmail.com>
2018-08-23 12:02:04 -0400

    update tsbasics

--- Molly_Merritt_Code/Weeks3and4/ts/tsbasics.ts
+++ Molly_Merritt_Code/Weeks3and4/ts/tsbasics.ts
@@ -46,3 +46,98 @@ let test2 = add('this is', ' a string');    // it would concatenate these string
 function addNum(a: number, b:number): number {
     return a + b;
 }
+
+function neverReturns(a: string): never {   // never returns
+    while(true) {}
+}
+
+function anyReturn(a: any): any {
+    return undefined;   // you can return anything for return type any
+}
+
+
+// TS supports Enum data type
+enum DaysOfWeek {
+    MONDAY = 0,
+    TUESDAY = 1,
+    WEDNESDAY = 2,
+    THURSDAY = 3,
+    FRIDAY = 4,
+    SATURDAY = 5,
+    SUNDAY = 6
+}
+
+let today = DaysOfWeek.THURSDAY;
+let tomorrow = DaysOfWeek[4];
+
+
+// ARROW NOTATION
+let sayHi = function(welcome: string) {
+    console.log(`${welcome}!!! ${9+10}`);   // can have mathematical expressions in `concatenation`
+}
+
+let sayHelloo = (welcome: string) => console.log(`${welcome}`);
+sayHelloo('hi');
+
+
+// INTERFACES
+interface Human {
+    name: string;
+    age: number;
+    hairColor?: string; // optional properties
+    speak(): void;  // function with no return type
+}
+
+let gen: Human = {
+    name: 'my name',
+    age: 100,
+    speak: ()=> {
+        console.log('arrow notation functionality uses ' +
+        'brackets and semicolons when there is more than one line of code');
+        console.log('TS is fun');
+    },
+    hairColor: 'brownish w overly bleached ends'
+}
+
+
+/*
+CLASSES
+*/
+class Point {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // in classes, functions are called methods
+
+    getDistance() { // don't say "function"
+        return Math.sqrt(this.x*this.x + this.y*this.y);    // have to say "this" here
+    }
+}
+
+let pointA = new Point(10, 10);
+console.log(pointA.getDistance());
+
+
+// INHERITANCE
+class Point3D extends Point {
+    z: number;
+
+    constructor(x: number, y:number, z:number) {
+        super(x,y); // MUST contain super call
+        this.z = z;
+    }
+
+    // overriding getDistance() method
+    getDistance() {
+        let dist = super.getDistance();
+        return Math.sqrt(dist*dist + this.z*this.z);
+    }
+}
+
+let pointZ = new Point3D(1, 2, 3);
+console.log(pointZ.getDistance());
