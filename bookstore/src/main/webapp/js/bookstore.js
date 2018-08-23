c9bcd62311c52768b49f46794665739d0fe4a68c
DailyDataMiner <benjaminmartin@theitguy.me>
2018-08-23 09:38:03 -0400

    Meh

--- bookstore/src/main/webapp/js/bookstore.js
+++ bookstore/src/main/webapp/js/bookstore.js
@@ -18,12 +18,47 @@ function loadHomeView(){
 		//console.log(xhr.responseText);
 		if(xhr.readyState == 4 && xhr.status == 200){
 			$('#view').html(xhr.responseText);
+			//once view is loaded, populate table and add eventListeners
+			populateBookTable();
 		}
 		
 	}
 	xhr.open("GET", "home.view");
 	xhr.send();
 	
+}
+
+function populateBookTable() {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState == 4 && xhr.status == 200) {
+			var books = JSON.parse(xhr.responseText);
+			for( var b in books) {
+				addBook(b); //helper function to create elements
+			}
+		}
+	}
+}
+
+function addBook(b) {
+	var row = document.createElement("tr");
+	var cell1 = document.createElement("td");
+	var cell2 = document.createElement("td");
+	var cell3 = document.createElement("td");
+	var cell4 = document.createElement("td");
+	
+	cell1.innerHTML = b.isbn;
+	cell2.innerHTML = b.title;
+	cell3.innerHTML = b.price;
+	cell4.innerHTML = b.genreId;
+	
+	row.appendChild(cell1);
+	row.appendChild(cell2);
+	row.appendChild(cell3);
+	row.appendChild(cell4);
+	
+	document.getElementbyId("bookTable").appendChild(row);
+
 }
 
 function loadBooksView(){
