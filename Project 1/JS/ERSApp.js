578861703cd6117c34c8387c325793a73e5e4962
DailyDataMiner <benjaminmartin@theitguy.me>
2018-08-24 11:34:09 -0400

    Meh

--- Project 1/JS/ERSApp.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    $('#logIn').on('click', logIn);
-    $('#showRegView').on('click', showRegisterView);
-}
-
-function showRegisterView() {
-    $('#logIn').detach();
-    
-}
