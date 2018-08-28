2c2954f54e00715995033c6e6681fcc94bc2553f
DailyDataMiner <benjaminmartin@theitguy.me>
2018-08-28 02:11:24 -0400

    Slowly failing at Project 1

--- ERS_Project/src/main/java/com/ex/servlets/LogInServlet.java
+++ ERS_Project/src/main/java/com/ex/servlets/LogInServlet.java
@@ -3,7 +3,6 @@ package com.ex.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,24 +13,30 @@ import com.ex.pojos.User;
 import com.ex.services.UserServices;
 
 public class LogInServlet extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
-
+	
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("login.html").forward(request, response);
+		//REQUEST DISPATCHER! Used for forwarding
+		System.out.println("Yo");
+		request.getRequestDispatcher("/login.html").forward(request, response);
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Fam");
 		response.setContentType("text/html");
+		User user;
 		
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		
-		System.out.println("LOGGING IN USER " + username + ":" + password);
+		System.out.println(username);
+		System.out.println(password);
 		
-		User user = UserServices.validation(username, password);
+		user = UserServices.validation(username, password);
 		
-		System.out.println(request);
-
 		if(user == null) {
 			response.sendRedirect("login.html");
 		}
@@ -40,14 +45,13 @@ public class LogInServlet extends HttpServlet {
 				HttpSession session = request.getSession();
 				session.setAttribute("user", user);
 				System.out.println(session.getId());
-				response.sendRedirect("/ERS_Project/EmployeeWelcome");
+				response.sendRedirect("EmployeeHome.html");
 			}
 			else if (user.getRoleid() == 110) {
 				HttpSession session = request.getSession();
 				session.setAttribute("user", user);
 				System.out.println(session.getId());
-				response.sendRedirect("/ERS_Project/ManagerWelcome");
-				// TODO: IMPLEMENT THIS
+				response.sendRedirect("ManagerHome.html");
 			}
 		}
 	}
