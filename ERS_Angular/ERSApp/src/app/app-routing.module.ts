dbbf01b9bec518e17b83b0974e6c8888fe7100e4
DailyDataMiner <benjaminmartin@theitguy.me>
2018-08-29 07:31:23 -0400

    Slowly figuring out angular.

--- ERS_Angular/ERSApp/src/app/app-routing.module.ts
+++ ERS_Angular/ERSApp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent
+    },
+
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- ERS_Angular/ERSApp/src/app/app.module.ts
+++ ERS_Angular/ERSApp/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
