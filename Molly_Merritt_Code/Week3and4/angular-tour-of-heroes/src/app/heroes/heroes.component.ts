bb3353034e6a564c8bf9ffd0eacddd90224b6ae9
mollymerritt <merritt.mry@gmail.com>
2018-08-30 21:54:35 -0400

    update tour-of-heroes

--- Molly_Merritt_Code/Week3and4/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Molly_Merritt_Code/Week3and4/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
